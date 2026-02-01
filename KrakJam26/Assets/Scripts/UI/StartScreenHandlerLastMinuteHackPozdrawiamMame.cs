766726185280271294e8c596bc6c013984b11b59
Krzyhau <contact@krzyhau.pl>
2026-02-01 13:48:51 +0100

    intro screen

---
+++ KrakJam26/Assets/Scripts/UI/StartScreenHandlerLastMinuteHackPozdrawiamMame.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreenHandlerLastMinuteHackPozdrawiamMame : MonoBehaviour
+{
+    [SerializeField] private GameObject cover1;
+    [SerializeField] private GameObject cover2;
+    [SerializeField] private GameObject cover3;
+    [SerializeField] private GameObject startTextCover;
+
+    [SerializeField] private float cover1Delay = 0.5f;
+    [SerializeField] private float cover2Delay = 1.0f;
+    [SerializeField] private float cover3Delay = 1.5f;
+    [SerializeField] private float startTextInterval = 2.0f;
+
+    [SerializeField] private AudioSource playerControllerThingy;
+    [SerializeField] private string sceneToLoad;
+
+    private void Update()
+    {
+        float t = playerControllerThingy != null && playerControllerThingy.isPlaying ? playerControllerThingy.time : 99999.999f;
+
+        cover1.SetActive(t < cover1Delay);
+        cover2.SetActive(t < cover2Delay);
+        cover3.SetActive(t < cover3Delay);
+        startTextCover.SetActive(Time.time % startTextInterval < startTextInterval * 0.5f);
+
+        bool anyPressed = false;
+        var kb = UnityEngine.InputSystem.Keyboard.current;
+        if (kb != null) anyPressed |= kb.anyKey.wasPressedThisFrame;
+
+        if (anyPressed)
+        {
+            TransitionFade.Transition(() => SceneManager.LoadScene(sceneToLoad));
+        }
+    }
+}
