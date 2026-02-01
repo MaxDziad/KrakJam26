2f31ad2bfa7e6df44f5f06333d85d428e60fd619
MaxPortablePC <dziadon.kakuzu@gmail.com>
2026-02-01 14:12:33 +0100

    Updating navmesh and enemies

--- KrakJam26/Assets/Scripts/Ai/AiSpawner.cs
+++ KrakJam26/Assets/Scripts/Ai/AiSpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,18 +6,37 @@ public class AiSpawner : MonoBehaviour
 {
 	private const float MIN_DISTANCE_FROM_PLAYER = 10f;
 
+	private float _spawnerCooldownTimer = 4f;
+	private bool _isOnCooldown = false;
+	private Coroutine _cooldownCoroutine;
+
 	[SerializeField]
 	private List<AiAgentType> _supportedAiAgentTypes = new List<AiAgentType>();
 
 	public bool CanSpawnThisAgent(AiAgentType agentType)
 	{
-		return IsFarEnoughFromPlayer() && _supportedAiAgentTypes.Contains(agentType);
+		return !_isOnCooldown && IsFarEnoughFromPlayer() && _supportedAiAgentTypes.Contains(agentType);
 	}
 
 	public void SpawnAiAgent(AiAgentBase aiAgentPrefab)
 	{
-		Debug.Log("spawn nastąpił");
 		Instantiate(aiAgentPrefab, transform.position, transform.rotation);
+
+		if (_cooldownCoroutine != null)
+		{
+			StopCoroutine(_cooldownCoroutine);
+			_cooldownCoroutine = null;
+		}
+
+		_isOnCooldown = true;
+		_cooldownCoroutine = StartCoroutine(Cooldown());
+	}
+
+	private IEnumerator Cooldown()
+	{
+		yield return new WaitForSeconds(_spawnerCooldownTimer);
+		_isOnCooldown = false;
+		_cooldownCoroutine = null;
 	}
 
 	private bool IsFarEnoughFromPlayer()
@@ -31,6 +51,12 @@ public class AiSpawner : MonoBehaviour
 
 	private void OnDestroy()
 	{
+		if (_cooldownCoroutine != null)
+		{
+			StopCoroutine(_cooldownCoroutine);
+			_cooldownCoroutine = null;
+		}
+
 		AiAgentSystemManager.Instance.UnregisterSpawner(this);
 	}
 }
